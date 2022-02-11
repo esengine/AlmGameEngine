a43549ad4a6155f46f9ea5576b809f55f4f3afd8
YHH <359807859@qq.com>
2022-02-11 10:47:05 +0800

    init commit

---
+++ Alm/buildDependices.bat
@@ -0,0 +1,6 @@
+cd Dependices/megasource
+git submodule init
+git submodule update
+git clone https://github.com/love2d/love libs/love
+cmake -G "Visual Studio 16 2019" -A Win32 -H. -Bbuild
+cmake --build build --target love/love --config Release
